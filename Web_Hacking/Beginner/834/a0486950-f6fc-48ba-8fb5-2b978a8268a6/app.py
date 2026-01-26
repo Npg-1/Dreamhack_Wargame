8a18b8f9f74aa8655f519f1de5d1d837ae707cd5
KimJungWoo <wjddn7118@naver.com>
2026-01-26 17:43:01 +0900

    834

--- Web_Hacking/Beginner/834/a0486950-f6fc-48ba-8fb5-2b978a8268a6/app.py
+++ Web_Hacking/Beginner/834/a0486950-f6fc-48ba-8fb5-2b978a8268a6/app.py
@@ -5,18 +5,23 @@ import re
 app = Flask(__name__)
 
 try:
-    FLAG = open("./flag.txt", "r").read()       # flag is here!, app.py는 현재 디렉토리의 flag.txt를 읽어들임
+    FLAG = open("./flag.txt", "r").read()
 except:
     FLAG = "[**FLAG**]"
 
-@app.route("/", methods = ["GET", "POST"])      # 사용자가 메인화면으로 들어가면 아래 함수를 수행
+@app.route("/", methods = ["GET", "POST"])
 def index():
     input_val = ""
     if request.method == "POST":
         input_val = request.form.get("input_val", "")
-        m = re.match(r'dr\w{5,7}e\d+am@[a-z]{3,7}\.\w+', input_val)
+        m = re.match(r'@[a-z]{3,7}\.\w+', input_val)     # dreeeeee0am@hack
+        # dreeeeee1am@hack\.lust
         if m:
             return render_template("index.html", pre_txt=input_val, flag=FLAG)
     return render_template("index.html", pre_txt=input_val, flag='?')
 
 app.run(host="0.0.0.0", port=8000)
+
+
+re.match(r'al\w{1,3}', input)
+
