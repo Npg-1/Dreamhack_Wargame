18f8502c56cd2f9b1e8bccf275d7793debeeaf00
KimJungWoo <wjddn7118@naver.com>
2026-02-11 17:56:02 +0900

    some

--- Web_Hacking/Level_2/2535/4da32adf-23bc-4fa5-b1cf-2eea9b206daf/내 index.js
+++ Web_Hacking/Level_2/2535/4da32adf-23bc-4fa5-b1cf-2eea9b206daf/내 index.js
@@ -9,34 +9,34 @@ const flag = successView.querySelector('.flag')
 const failView = document.querySelector('.failView')
 const questionRow = failView.querySelector('.questionRow')
 
-/*
-    [솔리데오SI시스템즈 Mr.Hwang 20050624]
-    관리자의 설정으로만 재응시 가능하게 수정
-*/
-// const resetButton = failView.querySelector('.resetButton')
+const resetButton = failView.querySelector('.resetButton')      // resetButton 요소를 html에서 가져오기
 
 const spinner = document.querySelector('.spinner')
 
 let currentQuestionId = null
 async function next(body) {
-    spinner.classList.remove('d-none')
-    questionView.classList.add('d-none')
-    successView.classList.add('d-none')
-    failView.classList.add('d-none')
-    const data = await fetch('/next', {
+    // classList는 요소의 클래스를 나타내는 것으로 classList.remove('d-none')는 요소의 클래스인 d-none을 삭제하게 됨
+    spinner.classList.remove('d-none')      // 여기서 말하는 d-none은 display: none 으로 remove를 했으니까 spinner 요소가 나타남
+    questionView.classList.add('d-none')    // 여기선 d-none을 추가했으니까  questionView가 안 보임
+    successView.classList.add('d-none')     // successView 안 보임
+    failView.classList.add('d-none')        // failView 안 보임
+
+    const data = await fetch('/next', {     // '/next'라는 주소로 body를 JSON형식으로 POPT 요청함
         method: 'post',
         body: JSON.stringify(body || {}),
-        headers: {
+        headers: {                          // 보내는 데이터가 JSON 형식이라는 걸 표시
             'Content-Type': 'application/json'
         }
-    }).then(res => res.json())
-    spinner.classList.add('d-none')
-    if (data.finished) {
-        if (data.incorrectQuestions.length === 0) {
+    }).then(res => res.json())              // 서버의 응답을 받아오면 받아온 응답(res)를 json으로 바꿔서 data에 저장
+
+    spinner.classList.add('d-none')         
+
+    if (data.finished) {                    // 서버에서 받아온 데이터에서 finished가 True라면
+        if (data.incorrectQuestions.length === 0) {         // 틀린 문제가 없다면, -> 모든 문제를 다 맞췄다면 플래그 출력
             successView.classList.remove('d-none')
             flag.textContent = data.flag
-        } else {
-            failView.classList.remove('d-none')
+        } else {                                            // 1개 이상 틀린 문제가 있다면 
+            failView.classList.remove('d-none')               
             let index = 1
             for (const questionData of data.incorrectQuestions) {
                 const questionElement = document.createElement('div')
@@ -48,12 +48,14 @@ async function next(body) {
                 questionRow.appendChild(questionElement)
             }
         }
-    } else if (data.question) {
-        questionView.classList.remove('d-none')
+    } 
+    else if (data.question) {   // 문제를 다 못 풀었다면
+        questionView.classList.remove('d-none')     // 문제 표시
         chapter.textContent = `${data.count + 1}. ${data.question.chapter || ''}`
         questionImage.src = data.question.questionImage
-        currentQuestionId = data.question.id
-    } else if (data.error) {
+        currentQuestionId = data.question.id        // 현재 문제 번호를 받아온 응답의 문제 번호로 기입함
+    } 
+    else if (data.error) {
         alert(data.error)
         location.reload()
     }
@@ -69,17 +71,30 @@ submitButton.addEventListener('click', () => {
     questionView.querySelector('input[name=questionRadio]:checked').checked = false
 })
 
+
+
 /*
     [솔리데오SI시스템즈 Mr.Hwang 20050624]
     관리자의 설정으로만 재응시 가능하게 수정
 */
-// resetButton.addEventListener('click', async () => {
-//     await fetch('/reset', {
-//         headers: {
-//             'dev-auth': '1q2w3e4r_solideo!!'
-//         }
-//     })
-//     location.reload()
-// })
 
-next()
+
+
+const data = await fetch('/next', {     // '/next'라는 주소로 body를 JSON형식으로 POPT 요청함
+    method: 'post',
+    body: JSON.stringify(body || {}),
+    headers: {                          // 보내는 데이터가 JSON 형식이라는 걸 표시
+        'Content-Type': 'application/json'
+    }
+}).then(res => res.json())              // 서버의 응답을 받아오면 받아온 응답(res)를 json으로 바꿔서 data에 저장
+
+
+
+resetButton.addEventListener('click', async () => {
+    await fetch('/reset', { headers: { 'dev-auth': '1q2w3e4r_solideo!!' } })
+    location.reload()
+})
+
+
+
+next()      // index.js가 처음 호출될 때 next()도 한 번은 호출하게 됨
