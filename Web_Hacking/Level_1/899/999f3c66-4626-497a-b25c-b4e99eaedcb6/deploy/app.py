ccc812a525b025eec9ae3e101dbc3a759ad26d9c
FLess <wjddn7118@gmail.com>
2026-03-04 00:36:17 +0900

    899 NOT YET..

--- Web_Hacking/Level_1/899/999f3c66-4626-497a-b25c-b4e99eaedcb6/deploy/app.py
+++ Web_Hacking/Level_1/899/999f3c66-4626-497a-b25c-b4e99eaedcb6/deploy/app.py
@@ -1,24 +1,19 @@
-# DH{55f2394156567886667940273839575eb0af4b3f115345f0cc8b9}
-# 55f239415656788666
+#!/usr/bin/env python3
+from flask import Flask, request, render_template
 
-## This is a sample FLAG ##
+app = Flask(__name__)
 
-# org = 0x55f239415656788666
-# org = (int)(org)
-# for i in range (0, 16):
-#     print("res == ", (int)(org / pow(2, 4 * i)) % 16)
+try:
+    FLAG = open("./flag.txt", "r").read()       # flag is here!
+except:
+    FLAG = "[**FLAG**]"
 
-# for i in range(12, 16):
-#     print(format(i, 'x'))
-
-
+@app.route('/', methods=['GET', 'POST'])
 def index():
     menu_str = ''
-    FLAG = "55f2394156567886667940273839575eb0af4b3f115345f0cc8b9"
-    org = FLAG[10:29]   # FLAG의 10~28번째 글자까지(10글자)를 org로 넣음
+    org = FLAG[10:29]   # FLAG의 10~28번째 글자까지를 org로 넣음
     org = int(org)      # 10진수 값
     st = ['' for i in range(16)]    # ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
-    print("org: ", org)
 
     for i in range (0, 16):
         res = (int)(org / pow(2, 4 * i)) % 16 # res는 4의 배수만큼 나눈 다음, 16의 나머지 값을 res에 할당함
@@ -31,37 +26,15 @@ def index():
             # 12~15 c, d, e, f (a와 b는 숫자로 들어가고 c, d, e, f만 알파벳으로 들어감)
             st[15-i] = format(res, 'x') # hex(res)와 값은 같은데 hex(res)는 앞에 0x 가 붙음
     menu_str = menu_str.join(st)
-    print(menu_str)
 
+    # POST
+    if request.method == "POST":
+        input_str =  request.form.get("menu_input", "")
+        if input_str == str(org):
+            return render_template('index.html', menu=menu_str, flag=FLAG)
+        return render_template('index.html', menu=menu_str, flag='try again...')
+    # GET
+    return render_template('index.html', menu=menu_str)
 
-index()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
